52267ba59cc69b40c5022973803de9b4eebcad19
Dominik Koch <dominik@koch-bautechnik.de>
2025-03-14 10:58:53 +0100

    refactor(routes): remove unused ProfileEditComponent route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './auth/guards/auth.guard';
 import { SearchComponent } from './pages/search/search.component';
 import { CreateEventsComponent } from './pages/events/create/create-events.component';
-import { ProfileEditComponent } from './pages/profile/edit/profile-edit.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -21,7 +20,6 @@ export const routes: Routes = [
   },
   {
     path: 'profile/edit',
-    component: ProfileEditComponent,
     canActivate: [authGuard],
   },
   { path: 'events/create', component: CreateEventsComponent },

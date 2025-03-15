e158a7bdd65b54937b37295754a184da0da0da6c
lvlina91 <149717925+lvlina91@users.noreply.github.com>
2025-03-15 21:33:36 +0100

    profile

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './auth/guards/auth.guard';
 import { SearchComponent } from './pages/search/search.component';
 import { CreateEventsComponent } from './pages/events/create/create-events.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -19,4 +20,5 @@ export const routes: Routes = [
     canActivate: [authGuard],
   },
   { path: 'events/create', component: CreateEventsComponent },
+  { path: 'profile', component: ProfileComponent },
 ];

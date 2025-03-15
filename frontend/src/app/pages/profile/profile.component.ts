d856b77a1d6bdfd8d49f1b44da8cf044cf984497
lvlina91 <149717925+lvlina91@users.noreply.github.com>
2025-03-15 23:05:02 +0100

    connect to backend

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-profile',
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  protected auth = inject(AuthService);
+  protected isLoggedIn = this.auth.isAuthenticated();
 
+  protected fullName: string | null = null;
+  protected userID: string | null = null;
+
+  ngOnInit() {
+    this.fullName = this.auth.getFullName();
+    this.userID = this.auth.getUserId();
+  }
 }

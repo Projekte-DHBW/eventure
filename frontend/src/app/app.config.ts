72a8b835270486ebfc38a4bb65fc4c6631e09f5b
Dominik Koch <kochd.tin24@student.dhbw-heidenheim.de>
2025-03-26 18:50:58 +0100

    fix: make slider work again (#55)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -8,6 +8,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import localeDe from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeDe);
 
 export const appConfig: ApplicationConfig = {
   providers: [
